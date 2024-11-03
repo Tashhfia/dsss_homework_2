09ca3ccbce0e47709b46752dca17479b4592b626
tashfia <hridishamim128@gmail.com>
2024-11-03 02:02:29 +0100

    setup

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name="math_quiz",
     version="0.1.0",
     description="A Python package for generating random math problems and quizzes.",
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+        entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz'
+        ],}
 )
